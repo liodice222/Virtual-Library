e34869afb69d7f9af619a78a59e191d3f9430523
Lea Iodice <liodice@deloitte.com>
2024-03-12 16:15:10 -0400

    added media and book.js

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,28 @@
 // import the Media class:
+const Media = require('./Media')
 
 // create your Book class:
+class Book extends Media {
+    constructor(author, numPages, rating){
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+        super(title);
+        super(year);
+        super(this.genre);
+    }
+    summary(){
+        return `Title: ${this.title}, Author: ${author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+     static highestRating (){
+        highest = Media.rating[0]
+        for (i = 0; i < Media.rating.length; i++){
+            if (Media.rating[i] > highest ){
+                highest = Media.rating[i]
+            }
+        }
+    }
+}
 
 // don't change below
 module.exports = Book;
--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,11 @@
 // import the Media class:
-
+const Media = require('./classes/Media')
 // create your Movie class:
+class Movie extends Media {
+    constructor(){
+        
+    }
+}
 
 // don't change below
 module.exports = Movie;
